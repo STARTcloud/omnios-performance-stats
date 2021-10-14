57c7ed2c2a03302cce728f49850969a9ca1dc99f
JoelProminic <joel@prominic.net>
2021-10-14 16:10:27 -0500

    Added command to cleanup hanging rwtop_to_csv instances.

--- scripts/rwtop_to_csv.sh
+++ scripts/rwtop_to_csv.sh
@@ -2,4 +2,12 @@
 
 export PATH=/usr/bin:/usr/sbin:/sbin:/usr/gnu/bin
 cd /opt/prominic/performance-stats/bin
+
+# Kill any existing instances of this process.  Example:
+#     root  8218  8215   0   Jun 30 ?          65:39 java -cp .:groovy-3.0.3.jar rwtop_to_csv 10
+# These processes are handing on a dtrace command.
+# Use a regular expression to make it less likely that I match an unrelated process
+pkill -f 'java.*rwtop_to_csv'
+
+
 java -cp ".:groovy-3.0.3.jar" rwtop_to_csv 10 >/tmp/rwtop_to_csv.log 2>&1 || true
